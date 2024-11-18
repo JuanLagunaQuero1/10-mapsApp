3caf7f867f56c34ea69c7cf10750d9cd23e7bcf7
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-11-18 12:10:06 +0100

    Control de Zoom y uso de Latitud y Longitud

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { LngLat, Map, MapStyle } from '@maptiler/sdk';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('map')
+  private divMap?: ElementRef<HTMLElement>;
+
+  public zoom: number = 11;
+  public map?: Map;
+  public currentCenter: LngLat = new LngLat( -3.79, 37.77 )
+
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'Elemento HTML no encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: MapStyle.STREETS,
+      center: this.currentCenter,
+      zoom: this.zoom,
+      navigationControl: false,
+      geolocateControl: false,
+      doubleClickZoom: false
+    });
+
+    this.mapListeners();
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
+  }
+
+  mapListeners() {
+    if ( !this.map ) throw 'Mapa no inicializado'
+
+    this.map.on('zoom', () => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', () => {
+      if (this.map!.getZoom() < 21) return;
+      this.map?.zoomTo(21);
+    });
+
+    this.map.on('zoomend', () => {
+      if (this.map!.getZoom() > 0.5) return;
+      this.map?.zoomTo(0.5);
+    });
+
+    this.map.on('move', () => {
+      this.currentCenter = this.map!.getCenter();
+    })
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value: string ) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 
 }
