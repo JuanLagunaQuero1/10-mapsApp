6b4ade9e3f49ce5ae5ff6a369fd897fa2c15359d
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-11-19 13:00:21 +0100

    Creación de minimapas para las properties

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, MapStyle, Marker } from '@maptiler/sdk';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input()
+  lngLat?: [number, number]
+
+  @ViewChild('map')
+  private divMap?: ElementRef<HTMLElement>;
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap?.nativeElement ) throw 'Map Div not found';
+    if ( !this.lngLat ) throw "LngLat can't be nul";
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: MapStyle.STREETS,
+      center: this.lngLat,
+      zoom: 15,
+      navigationControl: false,
+      interactive: false
+    });
+
+    new Marker()
+    .setLngLat( this.lngLat )
+    .addTo(map)
+
+  }
 
 }
